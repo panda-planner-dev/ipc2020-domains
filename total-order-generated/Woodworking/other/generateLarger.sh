031adc042948c7515fc3d7ce8959e679778b8581
Galvus Damor <behnkeg@informatik.uni-freiburg.de>
2020-10-08 17:57:46 +0200

    Woodworking larger instances

---
+++ total-order-generated/Woodworking/other/generateLarger.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## 
+
+python create_woodworking_instance.py 1 5 2 142 > ../problems/12.hddl
+python create_woodworking_instance.py 1 5 3 143 > ../problems/13.hddl
+python create_woodworking_instance.py 1 7 3 144 > ../problems/14.hddl
+python create_woodworking_instance.py 1 7 5 145 > ../problems/15.hddl
+python create_woodworking_instance.py 1 9 5 146 > ../problems/16.hddl
+python create_woodworking_instance.py 1 9 9 147 > ../problems/17.hddl
+python create_woodworking_instance.py 1 15 15 148 > ../problems/18.hddl
+python create_woodworking_instance.py 1 20 15 149 > ../problems/19.hddl
+python create_woodworking_instance.py 1 25 15 150 > ../problems/20.hddl
+python create_woodworking_instance.py 1 30 15 151 > ../problems/21.hddl
+python create_woodworking_instance.py 1 35 15 152 > ../problems/22.hddl
+python create_woodworking_instance.py 1 40 15 153 > ../problems/23.hddl
+python create_woodworking_instance.py 1 45 15 154 > ../problems/24.hddl
+python create_woodworking_instance.py 1 50 15 155 > ../problems/25.hddl
+python create_woodworking_instance.py 1 55 15 156 > ../problems/26.hddl
+python create_woodworking_instance.py 1 60 15 157 > ../problems/27.hddl
+python create_woodworking_instance.py 1 65 15 158 > ../problems/28.hddl
+python create_woodworking_instance.py 1 70 15 159 > ../problems/29.hddl
+python create_woodworking_instance.py 1 75 15 160 > ../problems/30.hddl
