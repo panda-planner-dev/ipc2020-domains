22914e79f3b6f95e5bb4cb1bfd326d6c8b04f70d
Galvus Damor <behnkeg@informatik.uni-freiburg.de>
2020-10-08 18:37:14 +0200

    Larger Childsnack instances

---
+++ total-order/Childsnack/other/generateLarger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## 
+
+python child-snack-generator.py pool 42 30 5 0.6 1.0 > ../problems/p21.hddl
+python child-snack-generator.py pool 42 35 5 0.6 1.0 > ../problems/p22.hddl
+python child-snack-generator.py pool 42 40 5 0.6 1.0 > ../problems/p23.hddl
+python child-snack-generator.py pool 42 50 5 0.6 1.0 > ../problems/p24.hddl
+python child-snack-generator.py pool 42 60 5 0.6 1.0 > ../problems/p25.hddl
+python child-snack-generator.py pool 42 70 5 0.6 1.0 > ../problems/p26.hddl
+python child-snack-generator.py pool 42 80 5 0.6 1.0 > ../problems/p27.hddl
+python child-snack-generator.py pool 42 90 5 0.6 1.0 > ../problems/p28.hddl
+python child-snack-generator.py pool 42 100 5 0.6 1.0 > ../problems/p29.hddl
+python child-snack-generator.py pool 42 110 5 0.6 1.0 > ../problems/p30.hddl
