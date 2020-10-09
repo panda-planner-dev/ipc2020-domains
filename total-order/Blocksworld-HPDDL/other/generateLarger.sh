ecfadf5f0bdf52338bfacd043ac6f7bb1c29a035
Galvus Damor <behnkeg@informatik.uni-freiburg.de>
2020-10-09 13:42:48 +0200

    Fixed type name

---
+++ total-order/Blocksworld-HPDDL/other/generateLarger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+make
+
+./blocksworld 3 50 > ../problems/p21.hddl
+./blocksworld 3 75 > ../problems/p22.hddl
+./blocksworld 3 100 > ../problems/p23.hddl
+./blocksworld 3 150 > ../problems/p24.hddl
+./blocksworld 3 200 > ../problems/p25.hddl
+./blocksworld 3 300 > ../problems/p26.hddl
+./blocksworld 3 400 > ../problems/p27.hddl
+./blocksworld 3 600 > ../problems/p28.hddl
+./blocksworld 3 800 > ../problems/p29.hddl
+./blocksworld 3 1000 > ../problems/p30.hddl
