11e49988bc20dd545e5674d14353b406128647c0
Galvus Damor <behnkeg@informatik.uni-freiburg.de>
2020-10-30 17:00:46 +0100

    Generator script

---
+++ total-order/Rover-GTOHP/other/generateLarger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## P20 has 12 65 9 12 ~13
+
+./rovgen 42 13 70 10 14 15 > ../problems/p21.hddl
+./rovgen 43 14 75 10 14 17 > ../problems/p22.hddl
+./rovgen 44 15 80 11 15 19 > ../problems/p23.hddl
+./rovgen 45 16 85 11 15 21 > ../problems/p24.hddl
+./rovgen 46 17 90 12 16 23 > ../problems/p25.hddl
+./rovgen 47 18 95 13 16 25 > ../problems/p26.hddl
+./rovgen 48 19 100 14 17 27 > ../problems/p27.hddl
+./rovgen 49 21 110 15 17 29 > ../problems/p28.hddl
+./rovgen 50 23 120 16 18 32 > ../problems/p29.hddl
+./rovgen 51 25 130 17 18 35 > ../problems/p30.hddl
